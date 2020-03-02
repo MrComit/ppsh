9f79c2c87152b7aea6f9b633617d3a8cab57e5cd
Cheezepin <cheezepin@gmail.com>
2020-03-01 17:22:45 -0800

    deleted sa and moved blend to level folder

--- levels/sa/header.inc.h
+++
@@ -1,7 +0,0 @@
-extern const GeoLayout sa_area_1_root[];
-extern const GeoLayout sa_area_1_level[];
-extern const Collision sa_area_1_collision[];
-extern const MacroObject sa_area_1_1_macro_objs[];
-extern const Gfx sa_visual_mesh[];
-extern const Gfx sa_material_revert_render_settings[];
-
