e017e935367ed50782081314dfb7fe6c3ed8623e
MrComit <yozemight@gmail.com>
2020-09-05 00:32:13 -0400

    simp city vertex coloring!!!

--- levels/jrb/texscroll.inc.c
+++ levels/jrb/texscroll.inc.c
@@ -1,3 +1,603 @@
+void scroll_jrb_dl_Cone_mesh_vtx_0() {
+	int i = 0;
+	int count = 9;
+	int width = 32 * 0x20;
+	int height = 32 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(jrb_dl_Cone_mesh_vtx_0);
+
+	deltaX = (int)(0.15000000596046448 * 0x20) % width;
+	deltaY = (int)(0.5 * 0x20) % height;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentX += deltaX;
+	currentY += deltaY;
+
+}
+void scroll_jrb_dl_Cone_001_mesh_vtx_0() {
+	int i = 0;
+	int count = 9;
+	int width = 32 * 0x20;
+	int height = 32 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(jrb_dl_Cone_001_mesh_vtx_0);
+
+	deltaX = (int)(0.15000000596046448 * 0x20) % width;
+	deltaY = (int)(0.5 * 0x20) % height;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentX += deltaX;
+	currentY += deltaY;
+
+}
+void scroll_jrb_dl_Cone_002_mesh_vtx_0() {
+	int i = 0;
+	int count = 9;
+	int width = 32 * 0x20;
+	int height = 32 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(jrb_dl_Cone_002_mesh_vtx_0);
+
+	deltaX = (int)(0.15000000596046448 * 0x20) % width;
+	deltaY = (int)(0.5 * 0x20) % height;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentX += deltaX;
+	currentY += deltaY;
+
+}
+void scroll_jrb_dl_Cone_003_mesh_vtx_0() {
+	int i = 0;
+	int count = 9;
+	int width = 32 * 0x20;
+	int height = 32 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(jrb_dl_Cone_003_mesh_vtx_0);
+
+	deltaX = (int)(0.15000000596046448 * 0x20) % width;
+	deltaY = (int)(0.5 * 0x20) % height;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentX += deltaX;
+	currentY += deltaY;
+
+}
+void scroll_jrb_dl_Cone_004_mesh_vtx_0() {
+	int i = 0;
+	int count = 9;
+	int width = 32 * 0x20;
+	int height = 32 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(jrb_dl_Cone_004_mesh_vtx_0);
+
+	deltaX = (int)(0.15000000596046448 * 0x20) % width;
+	deltaY = (int)(0.5 * 0x20) % height;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentX += deltaX;
+	currentY += deltaY;
+
+}
+void scroll_jrb_dl_Cone_005_mesh_vtx_0() {
+	int i = 0;
+	int count = 9;
+	int width = 32 * 0x20;
+	int height = 32 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(jrb_dl_Cone_005_mesh_vtx_0);
+
+	deltaX = (int)(0.15000000596046448 * 0x20) % width;
+	deltaY = (int)(0.5 * 0x20) % height;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentX += deltaX;
+	currentY += deltaY;
+
+}
+void scroll_jrb_dl_Cone_006_mesh_vtx_0() {
+	int i = 0;
+	int count = 9;
+	int width = 32 * 0x20;
+	int height = 32 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(jrb_dl_Cone_006_mesh_vtx_0);
+
+	deltaX = (int)(0.15000000596046448 * 0x20) % width;
+	deltaY = (int)(0.5 * 0x20) % height;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentX += deltaX;
+	currentY += deltaY;
+
+}
+void scroll_jrb_dl_Cone_007_mesh_vtx_0() {
+	int i = 0;
+	int count = 9;
+	int width = 32 * 0x20;
+	int height = 32 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(jrb_dl_Cone_007_mesh_vtx_0);
+
+	deltaX = (int)(0.15000000596046448 * 0x20) % width;
+	deltaY = (int)(0.5 * 0x20) % height;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentX += deltaX;
+	currentY += deltaY;
+
+}
+void scroll_jrb_dl_Cone_008_mesh_vtx_0() {
+	int i = 0;
+	int count = 9;
+	int width = 32 * 0x20;
+	int height = 32 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(jrb_dl_Cone_008_mesh_vtx_0);
+
+	deltaX = (int)(0.15000000596046448 * 0x20) % width;
+	deltaY = (int)(0.5 * 0x20) % height;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentX += deltaX;
+	currentY += deltaY;
+
+}
+void scroll_jrb_dl_Cone_009_mesh_vtx_0() {
+	int i = 0;
+	int count = 9;
+	int width = 32 * 0x20;
+	int height = 32 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(jrb_dl_Cone_009_mesh_vtx_0);
+
+	deltaX = (int)(0.15000000596046448 * 0x20) % width;
+	deltaY = (int)(0.5 * 0x20) % height;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentX += deltaX;
+	currentY += deltaY;
+
+}
+void scroll_jrb_dl_Cone_010_mesh_vtx_0() {
+	int i = 0;
+	int count = 9;
+	int width = 32 * 0x20;
+	int height = 32 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(jrb_dl_Cone_010_mesh_vtx_0);
+
+	deltaX = (int)(0.15000000596046448 * 0x20) % width;
+	deltaY = (int)(0.5 * 0x20) % height;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentX += deltaX;
+	currentY += deltaY;
+
+}
+void scroll_jrb_dl_Cone_011_mesh_vtx_0() {
+	int i = 0;
+	int count = 9;
+	int width = 32 * 0x20;
+	int height = 32 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(jrb_dl_Cone_011_mesh_vtx_0);
+
+	deltaX = (int)(0.15000000596046448 * 0x20) % width;
+	deltaY = (int)(0.5 * 0x20) % height;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentX += deltaX;
+	currentY += deltaY;
+
+}
+void scroll_jrb_dl_Cone_012_mesh_vtx_0() {
+	int i = 0;
+	int count = 9;
+	int width = 32 * 0x20;
+	int height = 32 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(jrb_dl_Cone_012_mesh_vtx_0);
+
+	deltaX = (int)(0.15000000596046448 * 0x20) % width;
+	deltaY = (int)(0.5 * 0x20) % height;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentX += deltaX;
+	currentY += deltaY;
+
+}
+void scroll_jrb_dl_Cone_013_mesh_vtx_0() {
+	int i = 0;
+	int count = 9;
+	int width = 32 * 0x20;
+	int height = 32 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(jrb_dl_Cone_013_mesh_vtx_0);
+
+	deltaX = (int)(0.15000000596046448 * 0x20) % width;
+	deltaY = (int)(0.5 * 0x20) % height;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentX += deltaX;
+	currentY += deltaY;
+
+}
+void scroll_jrb_dl_Cone_014_mesh_vtx_0() {
+	int i = 0;
+	int count = 9;
+	int width = 32 * 0x20;
+	int height = 32 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(jrb_dl_Cone_014_mesh_vtx_0);
+
+	deltaX = (int)(0.15000000596046448 * 0x20) % width;
+	deltaY = (int)(0.5 * 0x20) % height;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentX += deltaX;
+	currentY += deltaY;
+
+}
+void scroll_jrb_dl_Cone_015_mesh_vtx_0() {
+	int i = 0;
+	int count = 9;
+	int width = 32 * 0x20;
+	int height = 32 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(jrb_dl_Cone_015_mesh_vtx_0);
+
+	deltaX = (int)(0.15000000596046448 * 0x20) % width;
+	deltaY = (int)(0.5 * 0x20) % height;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentX += deltaX;
+	currentY += deltaY;
+
+}
+void scroll_jrb_dl_Cone_016_mesh_vtx_0() {
+	int i = 0;
+	int count = 9;
+	int width = 32 * 0x20;
+	int height = 32 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(jrb_dl_Cone_016_mesh_vtx_0);
+
+	deltaX = (int)(0.15000000596046448 * 0x20) % width;
+	deltaY = (int)(0.5 * 0x20) % height;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentX += deltaX;
+	currentY += deltaY;
+
+}
+void scroll_jrb_dl_Cone_017_mesh_vtx_0() {
+	int i = 0;
+	int count = 9;
+	int width = 32 * 0x20;
+	int height = 32 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(jrb_dl_Cone_017_mesh_vtx_0);
+
+	deltaX = (int)(0.15000000596046448 * 0x20) % width;
+	deltaY = (int)(0.5 * 0x20) % height;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentX += deltaX;
+	currentY += deltaY;
+
+}
+void scroll_jrb_dl_Cone_018_mesh_vtx_0() {
+	int i = 0;
+	int count = 9;
+	int width = 32 * 0x20;
+	int height = 32 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(jrb_dl_Cone_018_mesh_vtx_0);
+
+	deltaX = (int)(0.15000000596046448 * 0x20) % width;
+	deltaY = (int)(0.5 * 0x20) % height;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentX += deltaX;
+	currentY += deltaY;
+
+}
+void scroll_jrb_dl_Cone_019_mesh_vtx_0() {
+	int i = 0;
+	int count = 9;
+	int width = 32 * 0x20;
+	int height = 32 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(jrb_dl_Cone_019_mesh_vtx_0);
+
+	deltaX = (int)(0.15000000596046448 * 0x20) % width;
+	deltaY = (int)(0.5 * 0x20) % height;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentX += deltaX;
+	currentY += deltaY;
+
+}
 void scroll_jrb_dl_Sprinkler_mesh_vtx_0() {
 	int i = 0;
 	int count = 78;
@@ -83,6 +683,26 @@ void scroll_jrb_dl_ZZZZZZZZZZZ_003_mesh_vtx_0() {
 
 }
 void scroll_jrb() {
+	scroll_jrb_dl_Cone_mesh_vtx_0();
+	scroll_jrb_dl_Cone_001_mesh_vtx_0();
+	scroll_jrb_dl_Cone_002_mesh_vtx_0();
+	scroll_jrb_dl_Cone_003_mesh_vtx_0();
+	scroll_jrb_dl_Cone_004_mesh_vtx_0();
+	scroll_jrb_dl_Cone_005_mesh_vtx_0();
+	scroll_jrb_dl_Cone_006_mesh_vtx_0();
+	scroll_jrb_dl_Cone_007_mesh_vtx_0();
+	scroll_jrb_dl_Cone_008_mesh_vtx_0();
+	scroll_jrb_dl_Cone_009_mesh_vtx_0();
+	scroll_jrb_dl_Cone_010_mesh_vtx_0();
+	scroll_jrb_dl_Cone_011_mesh_vtx_0();
+	scroll_jrb_dl_Cone_012_mesh_vtx_0();
+	scroll_jrb_dl_Cone_013_mesh_vtx_0();
+	scroll_jrb_dl_Cone_014_mesh_vtx_0();
+	scroll_jrb_dl_Cone_015_mesh_vtx_0();
+	scroll_jrb_dl_Cone_016_mesh_vtx_0();
+	scroll_jrb_dl_Cone_017_mesh_vtx_0();
+	scroll_jrb_dl_Cone_018_mesh_vtx_0();
+	scroll_jrb_dl_Cone_019_mesh_vtx_0();
 	scroll_jrb_dl_Sprinkler_mesh_vtx_0();
 	scroll_jrb_dl_Sprinkler_001_mesh_vtx_0();
 	scroll_jrb_dl_ZZZZZZZZZZZ_003_mesh_vtx_0();
